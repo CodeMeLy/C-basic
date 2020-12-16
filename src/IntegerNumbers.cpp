e85c2535ab0029b60d947793e948c8cd73e7b4aa
thaitam <phithaitam@gmail.com>
2020-12-16 22:23:41 +0700

    # xuất giá trị max

--- src/IntegerNumbers.cpp
+++ src/IntegerNumbers.cpp
@@ -2,11 +2,14 @@
 #define MAXSIZE 50
 void enter(int numbers[],int &amount_numbers);
 void printValues(int numbers[], int amount_numbers);
+void printMaxValue(int numbers[], int amount_numbers);
+int findMaxValue(int numbers[], int amount_numbers);
 int main(){
     int numbers[MAXSIZE];
     int amount_numbers;
     enter(numbers,amount_numbers);// nhap các giá trị mảng
     printValues(numbers,amount_numbers);// xuat cac gia tri mang
+    printMaxValue(numbers,amount_numbers);// xuất giá trị max
 }
 void enter(int numbers[], int &amount_numbers){
     int temp;
@@ -24,6 +27,20 @@ void printValues(int numbers[], int amount_numbers){
     for (int index = 0; index < amount_numbers; index++)
     {
         printf(" %d",numbers[index]);
+    }    
+}
+void printMaxValue(int numbers[], int amount_numbers){
+    int max = findMaxValue(numbers,amount_numbers);
+    printf("\n Max of number is: %d",max);
+}
+int findMaxValue(int numbers[], int amount_numbers){
+    int firstIndex = 0;
+    int max = numbers[firstIndex];
+    for (int index = 1; index < amount_numbers; index++)
+    {
+        if(numbers[index]>max){
+            max = numbers[index];
+        }
     }
-    
+    return max;
 }
