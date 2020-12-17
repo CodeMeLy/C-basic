1077cb95c5eda2647e2c6ded2398037b01636986
thaitam <phithaitam@gmail.com>
2020-12-17 13:16:20 +0700

    tim khoang cach 2 diem

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,14 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 typedef struct Point{
     int x;
     int y;
 };
 void enter(Point &input);
-void printPoint(Point &output);
+float findDistance(Point start,Point end);
+void printPoint(Point output);
+void printDistance(Point start,Point end);
 int main(){
-    Point point;
-    enter(point);
-    printPoint(point);
+    Point start,end;
+    enter(start);
+    printPoint(start);
+    printf("\n");
+    enter(end);
+    printPoint(end);
+    printf("\n");
+    printDistance(start,end);
     return 0;
 }
 void enter(Point &input){
@@ -17,6 +25,13 @@ void enter(Point &input){
     printf(" y:");
     scanf("%d",&input.y);
 }
-void printPoint(Point &output){
+float findDistance(Point start,Point end){
+    return sqrt(pow(start.x-end.x,2) + pow(start.y-end.y,2));
+}
+void printPoint(Point output){
     printf("(%d,%d)",output.x,output.y);
+}
+void printDistance(Point start,Point end){
+    float distance = findDistance(start,end);
+    printf("\nDistance of 2 point: %.2f",distance);
 }
