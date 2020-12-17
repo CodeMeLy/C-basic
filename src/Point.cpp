3342f1e10af05c1701a6f9a4f7bb088a1eade98c
thaitam <phithaitam@gmail.com>
2020-12-17 13:07:23 +0700

    nhập, xuất 1 điểm

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,7 +1,22 @@
+#include<stdio.h>
 typedef struct Point{
     int x;
     int y;
 };
+void enter(Point &input);
+void printPoint(Point &output);
 int main(){
+    Point point;
+    enter(point);
+    printPoint(point);
     return 0;
+}
+void enter(Point &input){
+    printf("enter x:");
+    scanf("%d",&input.x);
+    printf(" y:");
+    scanf("%d",&input.y);
+}
+void printPoint(Point &output){
+    printf("(%d,%d)",output.x,output.y);
 }
